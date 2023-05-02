3f5fa2e7279afd78d16280ade722124f4f06d2a6
diegoivanpal <58339715+diegoivanpal@users.noreply.github.com>
2023-05-02 14:30:47 -0300

    probando subier imagenes

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Image from "next/image";
 import { useRouter } from "next/router";
 import {  useState } from "react";
 
@@ -7,13 +8,14 @@ export default function ProductForm({
     title: existingTitle,
     description: existingDescription,
     price: existingPrice,
-    images
+    images :existingImages
 
 }) {
     const [title, setTitle] = useState (existingTitle || '');
     const [description, setdescription] = useState (existingDescription ||'');
     const [price, setPrice] = useState (existingPrice || '');
     const [goToProducts, setGoToProducts] = useState(false)
+    const [images, setImages] = useState(existingImages || [])
     const router = useRouter()
 
     async function saveProduct(ev) {
@@ -37,16 +39,20 @@ export default function ProductForm({
         const files = ev.target?.files
         if (files?.length > 0) {
             const data = new FormData()
-            for (const file of files) {
+            
+            for (const file of files) {                 
+                //console.log(file)
                 data.append('file',file)
             }
             // const res =await axios.post('/api/upload',data, {
             //     headers: {'Content-type':'multipart/form-data'},
             // })
-            const res = await fetch('/api/upload', {
-                method: 'POST',
-                body: data,
-            } )
+            const res = await axios.post('/api/upload', data )
+
+            setImages(oldImages => { 
+                return [...oldImages, res.data.link]
+            })
+           // console.log(res.data.link)
             
         }
 
@@ -63,8 +69,13 @@ export default function ProductForm({
                     <label>
                         Photos 
                     </label>
-                    <div className="mb-2" > 
-                        <label className="w-24 h-24 cursor-pointer text-center flex items-center justify-center text-sm gap-1 text-gray-500 rounded-lg bg-gray-200">
+                    <div className="mb-2 flex flex-wrap gap-2" > 
+                    {!!images?.length && images.map((link,i) => (
+                        <div key={i} className=" h-24 w-24">
+                            <img src="http://localhost:3000/public/images/1683047236771_controller.png" alt='' className="rounded-lg"/>
+                        </div>
+                    ))}
+                        <label className=" w-24 h-24 cursor-pointer text-center flex items-center justify-center text-sm gap-1 text-gray-500 rounded-lg bg-gray-200">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
                         </svg>
--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,18 +1,35 @@
-import multiparty from 'multiparty'
+import formidable from 'formidable'
+import path from 'path'
+import fs from 'fs/promises'
+
 
 export default async function handle(req,res) {
-   const form = new multiparty.Form()
+    try {
+       // console.log("Read Directory")
+        await fs.readdir(path.join(process.cwd() +"/public","/images"))
+    } catch (error) {
+       // console.log("MkDir Directory")
+        await fs.mkdir(path.join(process.cwd() +"/public","/images"))
+    }
+   
+   const options = {}
+   options.uploadDir = path.join(process.cwd(), "/public/images")
+   options.filename = (name, ext, path, form) =>{ 
+    return Date.now().toString() + "_" +path.originalFilename
+   }
+
+   const form = formidable(options)
+
    const {fields,files} = await new Promise((resolve, reject) =>{
     form.parse(req, (err, fields, files) => {
         if (err) reject(err)
             resolve({fields,files})   
        })
-
    })
+    const link = files.file.filepath
+    console.log(files.file)
 
-   console.log('files:', files.file.length)
-   
-   return res.json('ok')
+   return res.status(200).json({link})
 
 }
 
