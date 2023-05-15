88c4c22f69685ea486fdcd6153254aaedd93f58c
diegoivanpal <58339715+diegoivanpal@users.noreply.github.com>
2023-05-15 13:46:25 -0300

    webhook

--- models/Product.js
+++ models/Product.js
@@ -7,6 +7,9 @@ const productShema = new Schema({
     images: [{type:String}],
     category: {type:mongoose.Types.ObjectId, ref:'Category'},
     properties: [{type:Object}]
+},
+{
+    timestamps: true,
 })
 
 export const Product = models.Product || model('Product', productShema)
