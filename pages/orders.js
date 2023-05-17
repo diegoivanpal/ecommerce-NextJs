909fa0f695a4d1dd4b550a6e1a41b8beccb21039
diegoivanpal <58339715+diegoivanpal@users.noreply.github.com>
2023-05-17 09:14:40 -0300

    complete

--- pages/orders.js
+++ pages/orders.js
@@ -17,6 +17,7 @@ export default function OrdersPage(){
                 <thead>
                     <tr>
                         <th>Date</th>
+                        <th>Paid</th>
                         <th>Recipient</th>
                         <th>Products</th>
                     </tr>
@@ -26,6 +27,9 @@ export default function OrdersPage(){
                         <tr key={order._id}>
                             <td>{new Date(order.createdAt).toLocaleString()
                             }</td>
+                            <td className={order.paid  ? 'text-gray-600' : 'text-red-600'}>
+                                {order.paid ? 'YES' : 'NO'}
+                            </td>
                             <td>{order.name}{order.email}<br/>
                             {order.city}{order.postCode}
                             {order.country}<br />
